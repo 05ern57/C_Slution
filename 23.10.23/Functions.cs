2a4eae898d605834bd2ea540c9d971472f8f9352
05ern57 <06ern57@gmail.com>
2023-10-24 16:39:32 +0300

    24.10.23 Ödev Son Aşama Hariç Yapıldı

--- 23.10.23/Functions.cs
+++ 23.10.23/Functions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Channels;
 using System.Threading.Tasks;
@@ -19,33 +20,12 @@ namespace _23._10._23
             return Math.Sqrt(csquare);
         }
 
-        public void Hipotenus()
-        {
-            Console.Write("a kenarının uzunluğunu giriniz : ");
-            double a = double.Parse(Console.ReadLine());
-            Console.Write("b kenarın uzunluğunu giriniz : ");
-            double b = double.Parse(Console.ReadLine());
-
-            double csquare = Math.Pow(a, 2) + Math.Pow(b, 2);
-            Console.WriteLine(Math.Sqrt(csquare));
-        }
 
         //Üçgenin Alanı
         public double AreaOfTriangle(double @base, double height)
         {
             double area = @base * height / 2;
             return area;
-        }
-
-        public void AreaOfTriangle()
-        {
-            Console.Write("taban uznuluğunu giriniz : ");
-            double @base = double.Parse(Console.ReadLine());
-            Console.Write("Yüksekliği giriniz : ");
-            double height = double.Parse(Console.ReadLine());
-
-            double area = @base * height / 2;
-            Console.WriteLine(area);
         }
 
         // Dairenin Alanı 
@@ -55,154 +35,228 @@ namespace _23._10._23
             return result;
         }
 
-        public void AreaOfCircle()
-        {
-            Console.Write("Dairenin yarıçapını girniz : ");
-            double radius = double.Parse(Console.ReadLine());
-
-            double result = Math.PI * Math.Pow(radius, 2);
-            Console.WriteLine(result);
-        }
-
         //Dikdörtgenin Alanı
         public double AreaOfRectangle(double @short, double @long)
         {
             return @short * @long;
         }
 
-        public void AreaOfRectangle()
-        {
-            Console.Write("Kısa kenarın uzunluğunu giriniz : ");
-            double @short = double.Parse(Console.ReadLine());
-            Console.Write("Uzun kenarın uzunluğunu giriniz : ");
-            double @long = double.Parse(Console.ReadLine());
-
-            Console.WriteLine( @short * @long);
-        }
-
         // Asal Sayı Kontrolü
-        public bool PrimeNumCtrl(int num)
+        public bool? PrimeNumCtrl(int num)
         {
             // 1'e  eşit veya küçük değilse 
             if (!(num <= 1))
             {
                 // ikiye eşitse 
                 if (num == 2) return true;
-                // burası dersteki işlemle aynı
-                for (int i = 3; i < (num / 2); i++)
+                // burası dersteki işlemle aynıydı fakat farklı bir şey denemek istedim.
+                bool? result = null;
+                for (int i = 3; i <= (num / 2); i++)
                 {
-                    // sayıya tam bölünüyorsa false döndürecek bölünmüyorsa true
-                    return (num % i == 0) ? false : true;
+                    // sayıya tam bölünüyorsa false döndürecek bölünmüyorsa 
+                    result = (num % i == 0) ? false : true;
                 }
+                return result;
+
             }
             return false;
         }
 
-        public void PrimeNumCtrl()
+        // Faktöriyel
+        public int? Factoriel(int num)
         {
+            // kendime not: değer döndüren fonksiyonlarda sadece if kısmıyla return yapıp bırakamazsın else kısmı olmadığında hatayla karşılaşıyorsun
+            if ((num < 0)) { return null; }
 
-            Console.Write("Asal olup olmadığını kontrol etmek istediğiniz sayıyı giriniz : ");
-            int num = int.Parse(Console.ReadLine());
-
-            // 1'e  eşit veya küçük değilse 
-            if (!(num <= 1))
+            if (!(num == 0 || num == 1))
             {
-                // ikiye eşitse 
-                if (num == 2) Console.WriteLine("Sayı Asal Sayıdır"); return;
-                // burası dersteki işlemle aynı
-                for (int i = 3; i < (num / 2); i++)
-                {
-                    // sayıya tam bölünüyorsa false döndürecek bölünmüyorsa true
-                    if (num % i == 0) { Console.WriteLine("Sayı Asal Sayı Değildir"); } 
-                    else { Console.WriteLine("Sayı Asal Sayıdır");}
-                }
+                return num * this.Factoriel(num - 1);
             }
-            Console.WriteLine("Sayı Asal Sayı Değildir");
-        }
-
-        // Faktöriyel Hesaplama
-        public short Factoriel(short num)
-        {
-            short result = num;
-            if (!(num <= 0))
+            else
             {
-                result *= Factoriel(--num);
+                return 1;
             }
-            return result;
-
-        }
-        // Bunu Çalıştıramadım
-        public void Factoriel()
-        {
-
-            Console.Write("Faktöriyelini öğrenmek istediğiniz sayıyı griniz : ");
-            int num = int.Parse(Console.ReadLine());
-
-            
-            int result = num;
-            for(int i =(num -1); num > 0; i--)
-            {
-                result *= i;
-            }
-            Console.WriteLine(result);
-
         }
 
         // Pozitif-Negatif kontrolü
         public string NumCtrl(int num)
         {
-            
-
             if (num != 0)
             {
                 return (num < 0) ? "Negatif" : "Pozitif";
             }
             else { return "Notr"; }
         }
-
-        public void NumCtrl()
-        {
-            Console.Write("Kontrol etmek istediğiniz Numarayı Giriniz : ");
-            int num = int.Parse(Console.ReadLine());
-
-            if (num != 0)
-            {
-                if (num < 0) { Console.WriteLine("Negatif Sayı"); } else { Console.WriteLine("Pozitif Sayı"); }
-            }
-            else { Console.WriteLine("Notr"); }
-
-        }
-
-        public void AllMethods()
-        {
-            // Verbatim String literal kullanımı ile işlem sorma 
-            Console.WriteLine(@"1.Hipotenüs Hesaplama
-2.)Üçgen Alanı Hesaplama
-3.)Dairenin Alanını Hesaplama
-4.)Dikdörtgenin Alanını Hesaplama
-5.)Asal Sayı Kontrolü
-6.)Faktöriyel Hesaplama
-7.)Pozitif-Negatif Sayı Kontrolü");
-
-            Console.Write("Yapmak istediğiniz işlem sayı ile belirtiniz:  ");
-            int transaction;
-            if (int.TryParse(Console.ReadLine(), out transaction))
-            {
-
-
-                switch (transaction)
-                {
-                    case 1: this.Hipotenus(); break;
-                    case 2: this.AreaOfTriangle(); break;
-                    case 3: this.AreaOfCircle(); break;
-                    case 4: this.AreaOfRectangle(); break;
-                    case 5: this.PrimeNumCtrl(); break;
-                    case 6: this.Factoriel(); break;
-                    case 7: this.NumCtrl(); break;
-                    default: Console.WriteLine("Uygun Olmayan Değer"); break;
-                }
-            }
-        }
-
     }
 }
+
+// ----------- Cw versiyonları-------------//
+
+//Console.WriteLine ile yapmayı denedim... (çalışıyor)
+//public void NumCtrl()
+//{
+//    Console.Write("Kontrol etmek istediğiniz Numarayı Giriniz : ");
+//    int num = int.Parse(Console.ReadLine());
+
+//    if (num != 0)
+//    {
+//        if (num < 0) { Console.WriteLine("Negatif Sayı"); } else { Console.WriteLine("Pozitif Sayı"); }
+//    }
+//    else { Console.WriteLine("Notr"); }
+
+//}
+
+//Console.WriteLine ile denedim.. (Çalışıyor)
+//public void PrimeNumCtrl()
+//{
+//    Console.Write("Asal olup olmadığını kontrol etmek istediğiniz sayıyı giriniz : ");
+//    int num = int.Parse(Console.ReadLine());
+
+//    string isNumPrime="İşlem Gerçekleştirilemedi";
+//    // 1'e  eşit veya küçük değilse 
+//    if(!(num <= 1))
+//    {
+//        for (int i = 3; i <= (num / 2); i++)
+//        {
+//            if(num % i == 0)
+//            {
+//                isNumPrime = "Asal Değildir";
+//            }
+//            else
+//            {
+//                isNumPrime = "Asaldır";
+//            }
+//        }
+//    }
+//    else
+//    {
+//        isNumPrime = "Asal Değildir";
+//    }
+//    Console.WriteLine($"Girmiş olduğunuz değer {isNumPrime}");
+//}
+
+//Console.WriteLine ile yaptım
+//public void AreaOfRectangle()
+//{
+//    Console.Write("Kısa kenarın uzunluğunu giriniz : ");
+//    double @short = double.Parse(Console.ReadLine());
+//    Console.Write("Uzun kenarın uzunluğunu giriniz : ");
+//    double @long = double.Parse(Console.ReadLine());
+
+//    Console.WriteLine( @short * @long);
+//}
+
+//Console.WriteLine ile yaptım
+//public void AreaOfCircle()
+//{
+//    Console.Write("Dairenin yarıçapını girniz : ");
+//    double radius = double.Parse(Console.ReadLine());
+
+//    double result = Math.PI * Math.Pow(radius, 2);
+//    Console.WriteLine(result);
+//}
+
+//Console.WriteLine ile yaptım
+//public void AreaOfTriangle()
+//{
+//    Console.Write("taban uznuluğunu giriniz : ");
+//    double @base = double.Parse(Console.ReadLine());
+//    Console.Write("Yüksekliği giriniz : ");
+//    double height = double.Parse(Console.ReadLine());
+
+//    double area = @base * height / 2;
+//    Console.WriteLine(area);
+//}
+
+//Console.WriteLine ile yaptım
+//public void Hipotenus()
+//{
+//    Console.Write("a kenarının uzunluğunu giriniz : ");
+//    double a = double.Parse(Console.ReadLine());
+//    Console.Write("b kenarın uzunluğunu giriniz : ");
+//    double b = double.Parse(Console.ReadLine());
+
+//    double csquare = Math.Pow(a, 2) + Math.Pow(b, 2);
+//    Console.WriteLine(Math.Sqrt(csquare));
+//}
+
+
+
+//       ################### Denemelerim #########################
+
+// 2. Maddenin hepsini functions.cs de bir method olarak tanımlamaya çalıştım. 
+
+//        public void AllMethods()
+//        {
+//            // Verbatim String literal kullanımı ile işlem sorma 
+//            Console.WriteLine(@"1.Hipotenüs Hesaplama
+//2.)Üçgen Alanı Hesaplama
+//3.)Dairenin Alanını Hesaplama
+//4.)Dikdörtgenin Alanını Hesaplama
+//5.)Asal Sayı Kontrolü
+//6.)Faktöriyel Hesaplama
+//7.)Pozitif-Negatif Sayı Kontrolü");
+
+//            Console.Write("Yapmak istediğiniz işlem sayı ile belirtiniz:  ");
+//            int transaction;
+//            if (int.TryParse(Console.ReadLine(), out transaction))
+//            {
+
+
+//                switch (transaction)
+//                {
+//                    case 1: this.Hipotenus(); break;
+//                    case 2: this.AreaOfTriangle(); break;
+//                    case 3: this.AreaOfCircle(); break;
+//                    case 4: this.AreaOfRectangle(); break;
+//                    case 5: this.PrimeNumCtrl(); break;
+//                    case 6: this.Factoriel(); break;
+//                    case 7: this.NumCtrl(); break;
+//                    default: Console.WriteLine("Uygun Olmayan Değer"); break;
+//                }
+//            }
+//        }
+
+
+
+// Burda da recursive metodu cw kullanarak yapmaya çalıştım.
+
+// Bunu Çalıştıramadım => çünkü recursive methodlar herzaman kendilerini geri döndürürler bunu Console.Writeline ile yapamayız
+//public void Factoriel()
+//{
+
+//    Console.Write("Faktöriyelini öğrenmek istediğiniz sayıyı griniz : ");
+//    int num = int.Parse(Console.ReadLine());
+
+//    if ((num < 0)) { Console.WriteLine("Kesirli Negatif Sayıların Faktöriyeli Olmaz"); }
+
+//    if (!(num == 0 || num == 1))
+//    {
+//        int? result = num * this.Factoriel(num - 1);
+//        Console.WriteLine($"İşlemin Sonucu {result}");
+//    }
+//    else
+//    {
+//        int? result = num * this.Factoriel(num - 1);
+//        Console.WriteLine($"İşlemin Sonucu {result}");
+//    };
+
+//    //Console.WriteLine(result);
+
+//}
+
+
+
+// Geri Döndürmeden yapma girişimi 2
+
+// Faktöriyel Hesaplama
+//public int Factoriel(int num)
+//{
+//    int result = num;
+//    if (!(num <= 0))
+//    {
+//        result *= Factoriel(--num);
+//    }
+//    return result;
+
+//}
--- 23.10.23/Program.cs
+++ 23.10.23/Program.cs
@@ -1,10 +1,88 @@
-﻿// 2.)Yazmış olduğum tüm metodların kullanılabileceği program
+﻿//// 2.)Yazmış olduğum tüm metodların kullanılabileceği program 
 
 
 using _23._10._23;
 
-Functions functions = new Functions();
+Functions functions = new Functions(); 
 
-functions.AllMethods();
 
-//Console.WriteLine(functions.Factoriel(5));
+while (true)
+{
+    Console.WriteLine(@"1.Hipotenüs Hesaplama
+2.)Üçgen Alanı Hesaplama
+3.)Dairenin Alanını Hesaplama
+4.)Dikdörtgenin Alanını Hesaplama
+5.)Asal Sayı Kontrolü
+6.)Faktöriyel Hesaplama
+7.)Pozitif-Negatif Sayı Kontrolü");
+
+    Console.Write("Yapmak istediğiniz işlem sayı ile belirtiniz:  ");
+    int transaction;
+    if (int.TryParse(Console.ReadLine(), out transaction))
+    {
+        switch (transaction)
+        {
+            case 1:
+                Console.Write("a kenarının uzunluğunu giriniz : ");
+                double a = double.Parse(Console.ReadLine());
+                Console.Write("b kenarın uzunluğunu giriniz : ");
+                double b = double.Parse(Console.ReadLine());
+
+                Console.WriteLine( functions.Hipotenus(a,b)); break;
+
+            case 2:
+
+                Console.Write("taban uznuluğunu giriniz : ");
+                double @base = double.Parse(Console.ReadLine());
+                Console.Write("Yüksekliği giriniz : ");
+                double height = double.Parse(Console.ReadLine());
+
+                Console.WriteLine(functions.AreaOfTriangle(@base, height)) ; break;
+
+            case 3:
+                Console.Write("Dairenin yarıçapını girniz : ");
+                double radius = double.Parse(Console.ReadLine());
+
+                Console.WriteLine(functions.AreaOfCircle(radius)); ; break;
+
+            case 4:
+                Console.Write("Kısa kenarın uzunluğunu giriniz : ");
+                double @short = double.Parse(Console.ReadLine());
+                Console.Write("Uzun kenarın uzunluğunu giriniz : ");
+                double @long = double.Parse(Console.ReadLine());
+
+                Console.WriteLine(functions.AreaOfRectangle(@short, @long)) ; break;
+            case 5:
+                Console.Write("Asal olup olmadığını kontrol etmek istediğiniz sayıyı giriniz : ");
+                int num1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(functions.PrimeNumCtrl(num1)); break;
+            case 6:
+                Console.Write("Faktöriyelini öğrenmek istediğiniz sayıyı griniz : ");
+                int num2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(functions.Factoriel(num2)); break;
+            case 7:
+                Console.Write("Kontrol etmek istediğiniz Numarayı Giriniz : ");
+                int num3 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(functions.NumCtrl(num3)) ; break;
+            default: Console.WriteLine("Uygun Olmayan Değer"); break;
+        }
+
+        Console.Write("Başka bir işlem yapmak istiyor musunuz? Evet ise 1 değil ise 2 yazın : ");
+        int isLoopContinue;
+        if (int.TryParse(Console.ReadLine(), out isLoopContinue) && isLoopContinue == 1)
+        {
+            Console.WriteLine("\n\n\n");
+            // devam etsin diye dokunmuyorum
+        }
+        else { break; }
+
+        
+    }
+    else
+    {
+        Console.WriteLine("Girilen Değer Uygun Bir Değer Değil Lütfen Tekrar Deneyiniz");
+    }
+}
